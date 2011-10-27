e9b3dd17003368bab1b91cf4487fe9a219603d70
Paweł Jędrzejewski <paweu.jedrzejewski@gmail.com>
2011-10-27 23:38:03 +0200

    basic blamer interface.

---
+++ Blamer/PostBlamerInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\BloggerBundle\Blamer;
+
+use Sylius\Bundle\BloggerBundle\Model\PostInterface;
+
+/**
+ * Interface for post blamer.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+interface PostBlamerInterface
+{
+    /**
+     * Sets user or username as post author.
+     * 
+     * @param PostInterface $post
+     */
+    function blame(PostInterface $post);
+}

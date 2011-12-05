85c0aba233995db78b0a9eb27a5b71530673cb2b
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2011-12-05 19:52:08 +0100

    added simple docs and default interface with Twitter Bootstrap.

---
+++ Entity/DefaultPost.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\BloggerBundle\Entity;
+
+use Sylius\Bundle\BloggerBundle\Entity\Post as BasePost;
+
+/**
+ * Default post entity.
+ * 
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class DefaultPost extends BasePost
+{    
+}
